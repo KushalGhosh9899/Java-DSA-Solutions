54c3fba64be9d8e91db6c41505957c0d8be4f33c
Kushal <kushalghosh9899@gmail.com>
2021-12-27 22:14:50 +0530

    Solved: Question 7
    Printing fibonacci series

---
+++ src/com/company/assignment_7.java
@@ -0,0 +1,24 @@
+//To calculate Fibonacci Series up to n numbers.
+package com.company;
+
+import java.util.Scanner;
+
+public class assignment_7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Enter the Number up to you wanted to print the series = ");
+        int num = input.nextInt();
+        System.out.println("Fibonacci Series of number "+ num +" is :-");
+        int prevterm1 = 0;
+        int prevterm2 = 1;
+        int newnum = 0;
+        System.out.print(prevterm1+", ");
+        System.out.print(prevterm2);
+        for (int i=1; newnum<num;i++){
+            newnum=prevterm1+prevterm2;
+            System.out.print(", "+newnum);
+            prevterm1=prevterm2;
+            prevterm2=newnum;
+        }
+    }
+}
