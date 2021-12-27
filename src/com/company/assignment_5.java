c939db67b2f6e6096fa6e02242cb8598cd846999
Kushal <kushalghosh9899@gmail.com>
2021-12-27 21:22:25 +0530

    Solved: Question 5
    Solved question number 5 from assignment 2

---
+++ src/com/company/assignment_5.java
@@ -0,0 +1,23 @@
+//Take 2 numbers as input and print the largest number
+package com.company;
+
+import java.util.Scanner;
+
+public class assignment_5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Enter the First Number = ");
+        int num1 = input.nextInt();
+        System.out.printf("Enter the Second Number = ");
+        int num2 = input.nextInt();
+        if(num1>num2){
+            System.out.printf("First Number is Greater");
+        }
+        else if(num2>num1){
+            System.out.printf("Second Number is Greater");
+        }
+        else{
+            System.out.printf("Both Number are equal");
+        }
+    }
+}
